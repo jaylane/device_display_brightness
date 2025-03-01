c570d589472fbf37e725c7b2a8ef61e9df6bf3ed
Jason Lane <jason.j.lane@gmail.com>
2025-03-01 02:53:27 -0800

    update tests

--- com.jlane.device_display_brightness/lib/device_display_brightness.dart
+++ com.jlane.device_display_brightness/lib/device_display_brightness.dart
@@ -1,58 +1,70 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
+/// A Flutter plugin to control display brightness and keep the screen on.
 class DeviceDisplayBrightness {
   static const MethodChannel _channel =
-      const MethodChannel('github.com/SVD13/device_display_brightness');
+      const MethodChannel('device_display_brightness');
 
-  /// Returns the display backlight brightness between 0 and 1.
-  static Future<double> getBrightness() async {
-    return await _channel.invokeMethod<double>('getBrightness') ?? 0;
+  /// Get the current brightness level of the device.
+  ///
+  /// Returns a value between 0.0 and 1.0, where 0.0 is the minimum
+  /// brightness and 1.0 is the maximum brightness.
+  static Future<double> get brightness async {
+    final double brightness = await _channel.invokeMethod('getBrightness');
+    return brightness;
   }
 
-  /// The value of [brightness] should be a number between 0.0 and 1.0, inclusive.
+  /// Set the brightness level of the device.
   ///
-  /// In iOS:
-  /// Brightness changes made by an app remain in effect until
-  /// the device is locked, regardless of whether the app is closed.
-  /// The system brightness (which the user can set in Settings or Control Center)
-  /// is restored the next time the display is turned on.
-  ///
-  /// In Android:
-  /// This method overrides system brightness while the app is in the foreground.
+  /// [brightness] should be a value between 0.0 and 1.0, where 0.0 is the minimum
+  /// brightness and 1.0 is the maximum brightness.
   static Future<void> setBrightness(double brightness) async {
-    if (Platform.isIOS || Platform.isAndroid) {
-      return _channel.invokeMethod(
-        'setBrightness',
-        {'brightness': brightness.clamp(0.0, 1.0)},
-      );
-    }
+    assert(brightness >= 0.0 && brightness <= 1.0);
+    await _channel.invokeMethod('setBrightness', {'brightness': brightness});
   }
 
-  /// In iOS:
-  /// Resets brightness to last known system value.
-  /// System value is fetched, when app starts.
+  /// Keep the screen on (prevent it from turning off due to inactivity).
   ///
-  /// In Android:
-  /// Resets brightness to system value.
-  static Future<void> resetBrightness() async {
-    if (Platform.isIOS || Platform.isAndroid) {
-      return await _channel.invokeMethod('resetBrightness');
+  /// [on] determines whether to keep the screen on (true) or allow it to turn off (false).
+  static Future<void> keepOn(bool on) async {
+    await _channel.invokeMethod('keepOn', {'on': on});
+  }
+
+  /// Check if the device has permission to change the brightness.
+  ///
+  /// Returns true if the app has permission to change the brightness,
+  /// or false otherwise. On iOS, this always returns true as no special
+  /// permission is required.
+  static Future<bool> get hasPermission async {
+    if (Platform.isIOS) {
+      return true;
     }
+    final bool hasPermission = await _channel.invokeMethod('hasPermission');
+    return hasPermission;
   }
 
-  /// Returns `true` if the device's screen is keeping turned on.
-  static Future<bool> isKeptOn() async {
-    return await _channel.invokeMethod<bool>('isKeptOn') ?? false;
+  /// Request permission to change the screen brightness.
+  ///
+  /// On Android, this will prompt the user to grant the WRITE_SETTINGS permission.
+  /// On iOS, this does nothing and returns true immediately as no special
+  /// permission is required.
+  ///
+  /// Returns true if the permission was granted, false otherwise.
+  static Future<bool> requestPermission() async {
+    if (Platform.isIOS) {
+      return true;
+    }
+    final bool granted = await _channel.invokeMethod('requestPermission');
+    return granted;
   }
 
-  /// If [enabled] == `true`, the device's screen will be keeping turned on.
-  static Future<void> keepOn({required bool enabled}) async {
-    return _channel.invokeMethod(
-      'keepOn',
-      {'enabled': enabled},
-    );
+  /// Get the current version of the plugin.
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
   }
 }
